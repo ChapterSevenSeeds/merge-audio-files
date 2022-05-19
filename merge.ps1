f258ab30cee132890241bf625d83b0b9b528d1af
Tyson Jones <ballisticmissle@gmail.com>
2022-05-18 18:51:03 -0600

    ffmpeg

--- merge.ps1
+++ merge.ps1
@@ -1,1 +1,3 @@
 Get-ChildItem -Path '.\Dream Theater\' -Recurse | Where { $_.PSIsFile} | Select FullName
+
+ffmpeg -i "03 Constant Motion.m4a" -i "04 The Dark Eternal Night.m4a" -i "02 This Dying Soul.mp3" -filter_complex amix=inputs=3:duration=longest output.mp3
